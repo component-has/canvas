bb3c0585b59156631fda44ffbe9b18187b10df60
Amir Abu Shareb <yields@icloud.com>
2013-06-26 02:42:21 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# canvas
+
+  canvas support check.
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install component-has/canvas
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ test/test.js
@@ -0,0 +1,9 @@
+
+describe('has-canvas', function(){
+  var canvas = require('canvas');
+  var assert = require('assert');
+
+  it('should be a boolean', function(){
+    assert('boolean' == typeof canvas);
+  })
+})
